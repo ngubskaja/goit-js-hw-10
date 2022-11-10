014e77d7ce3ade7735b2dc9f5cb32c8cd76740cd
Gubska Nastya <97690198+ngubskaja@users.noreply.github.com>
2022-11-10 19:32:07 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import {fetchCountries} from "./fetchCountries";
+
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+// import './fetchCountries'
+
+
+  fetchCountries('ukraine');
+
